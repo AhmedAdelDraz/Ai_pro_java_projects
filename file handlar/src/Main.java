e05e6883ac7bf46fe9f1f5266f44ea623bf40718
draz <ahmeddrax9@gmail.com>
2021-06-24 03:09:57 +0200

    reduce lines of code.

--- file handlar/src/Main.java
+++ file handlar/src/Main.java
@@ -7,29 +7,24 @@ import java.io.InputStreamReader;
 public class Main {
 	public static void main(String[] args) {
 		try {
-		InputStreamReader r = new InputStreamReader(System.in);
-		BufferedReader br = new BufferedReader(r);
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			BufferedWriter buffer = new BufferedWriter(new FileWriter("/home/draz/Desktop/java_output.txt"));
 		
-		FileWriter writer = new FileWriter("/home/draz/Desktop/java_output.txt");
-		BufferedWriter buffer = new BufferedWriter(writer);
-		
-		String line;
-		do {	
-			line = br.readLine();
-			if(!line.equalsIgnoreCase("stop")){
-					buffer.write(line+"\n");
+			String line;
+			do {	
+				line = br.readLine();
+				if(!line.equalsIgnoreCase("stop")){
+						buffer.write(line+"\n");
+				}
 			}
-		}
-		while(!line.equalsIgnoreCase("stop"));
+			while(!line.equalsIgnoreCase("stop"));
 		
-	
-		br.close();
-		buffer.close();
+			br.close();
+			buffer.close();
 		
 		} catch(IOException ex) {
 			ex.printStackTrace();
 		}
-		
 		
 	}
 
